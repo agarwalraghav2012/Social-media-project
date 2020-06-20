9638713163cc64964873a531017d6e1bef125681
Raghav Agarwal <raghav18177@iiitd.ac.in>
2020-06-20 18:14:51 +0530

    can write comments see full post
    Signed-off-by: Raghav Agarwal <raghav18177@iiitd.ac.in>

--- src/public/app/all-posts.js
+++ src/public/app/all-posts.js
@@ -13,8 +13,6 @@ function loadPosts() {
                   ${p.body.substr(0, 200)}
                   <a href="#" class="a1" dataId = ${p.id}>...read more</a>
                 </p>
-                <a href="#" class="card-link">Comment</a>
-                <a href="#" class="card-link">Like</a>
               </div>
             </div>
           </div>
@@ -27,25 +25,96 @@ function loadPosts() {
 
         $('#content').load("../components/comments.html") ;  
         var id = $(this).attr('dataId') ;
+   
           
         $.get(`/api/posts/comments/${id}`, (comments) => {
+
+          $.get(`/api/posts/${id}`, (post) => {
+            
+            $('#fullpost').append(
+              `
+              <div class="card text-center">
+              <div class="card-header">
+              <strong>${post[0].title}</strong>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">- ${post[0].user.username}</h5>
+                <p class="card-text">${post[0].body}</p>
+              </div>
+            </div>
+              `
+            )
+          }) ;
+
           for (let c of comments) {
                 
             $('#comments-container').append(
-                $(`
-                <div class="card" style="width: 90%;">
+                `
+                <div class="card" style="width: 60%;">
                 <div class="card-body">
                   <h5 class="card-title">${c.title}</h5>
                   <h6 class="card-subtitle mb-2 text-muted">- ${c.user.username}</h6>
                   <p class="card-text">${c.body}</p>
                 </div>
                 </div>
-                `)
-            )
-          }
-        })
+                `
+            ) ;
+          } 
+          
+          $('#formbody').append(
+            `
+            <h5> Write comment :-</h5>
+            <form>
+                <div class="form-group col-md-11">
+                  <label >Comment Title</label>
+                  <input type="text" class="form-control" id="title">
+                </div>
+                <div class="form-group col-md-11">
+                  <label >Details</label>
+                  <textarea class="form-control"  rows="2" id="body" ></textarea>
+                </div>
+                <div class="form-group form-check col-md-11">
+                  <input type="checkbox" class="form-check-input">
+                  <label class="form-check-label">  Allow us to show this content to other users as well</label>
+                </div>
+                <button type="submit" class="btn btn-primary" id="post"> Post </button>
+              </form>
+            `
+        ) ;
+
+        $("#post").click(function(){
+          
+          $.post('/api/posts/comments/', {
+              userId: window.currentUser.id ,
+              title: $('#title').val(),
+              body: $('#body').val(),
+              postId: id
+              }, 
+              function(data){
+          });
+        });
+
+        }) ;
+        
+
+        
+        /*$("#post").click(function(){
+          console.log("post func run");
+          
+          $.post('/api/posts/comments/', {
+              userId: window.currentUser.id ,
+              title: $('#title').val(),
+              body: $('#body').val(),
+              postId: id
+              }, 
+              function(data){
+                  console.log(data) ;
+          });
+        }); */
 
         });
+
+        
 
     })
   }
